0b6b6f8dfcf896c8a50963f4fc5f1ada6e60b228
Yona Mahundi <yona101992@gmail.com>
2022-02-27 18:59:19 +0300

    theming improvements

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -6,7 +6,6 @@ class CustomTheme {
   static final ThemeData baseDark = ThemeData.dark();
   static const Color primaryColor = Color(0xff65c45e);
 
-
   static ThemeData get darkTheme {
     return ThemeData(
       brightness: Brightness.dark,
@@ -24,7 +23,7 @@ class CustomTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       bottomAppBarColor: Colors.green[100],
-      cardColor:const Color(0xffbd5ec4) ,
+      cardColor: const Color(0xffbd5ec4),
       checkboxTheme: CheckboxThemeData(
         fillColor: MaterialStateProperty.all(
           const Color(0xffab02ee),
@@ -36,31 +35,53 @@ class CustomTheme {
     );
   }
 
-
-
-static ThemeData get kShrineTheme => _buildShrineTheme();
+  static ThemeData get kShrineTheme => _buildShrineTheme();
 
   static ThemeData _buildShrineTheme() {
     final ThemeData base = ThemeData.light();
     return base.copyWith(
-      // primaryColor: Colors.green,
       colorScheme: base.colorScheme.copyWith(
         primary: kShrinePink100,
         onPrimary: kShrineBrown900,
         secondary: kShrineBrown900,
         error: kShrineErrorRed,
       ),
-      // TODO: Add the text themes (103)
-      // TODO: Add the icon themes (103)
-      // TODO: Decorate the inputs (103)
+      textTheme: _buildShrineTextTheme(base.textTheme),
+      textSelectionTheme: const TextSelectionThemeData(
+        selectionColor: kShrinePink100,
+      ),
+      // inputDecorationTheme: const InputDecorationTheme(
+      //   focusedBorder: CutCornersBorder(
+      //     borderSide: BorderSide(
+      //       width: 2.0,
+      //       color: kShrineBrown900,
+      //     ),
+      //   ),
+      //   border: CutCornersBorder(),
     );
   }
 
-
+  static TextTheme _buildShrineTextTheme(TextTheme base) {
+    return base
+        .copyWith(
+          headline5: base.headline5!.copyWith(
+            fontWeight: FontWeight.w500,
+          ),
+          headline6: base.headline6!.copyWith(
+            fontSize: 18.0,
+          ),
+          caption: base.caption!.copyWith(
+            fontWeight: FontWeight.w400,
+            fontSize: 14.0,
+          ),
+          bodyText1: base.bodyText1!.copyWith(
+            fontWeight: FontWeight.w500,
+            fontSize: 16.0,
+          ),
+        )
+        .apply(
+          displayColor: kShrineBrown900,
+          bodyColor: kShrineBrown900,
+        );
+  }
 }
-
-
-
-
-
-
